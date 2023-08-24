d957cec4a7305a10f480e5f42b9a952f9a5e1738
Pavel Odintsov <pavel.odintsov@gmail.com>
2023-08-24 23:33:26 +0100

    Simplified reproduce example

--- reuseport.cpp
+++ reuseport.cpp
@@ -6,7 +6,7 @@
 #include <netdb.h>
 #include <linux/filter.h>
 
-void start_netflow_collector(const std::string& netflow_host, unsigned int netflow_port, bool reuse_port,  uint32_t netflow_threads_per_port) {
+void start_netflow_collector(const std::string& netflow_host, unsigned int netflow_port, uint32_t netflow_threads_per_port) {
     std::cout << "Netflow plugin will listen on " << netflow_host << ":" << netflow_port << " udp port" << std::endl;
 
     struct addrinfo hints;
@@ -33,46 +33,44 @@ void start_netflow_collector(const std::string& netflow_host, unsigned int netfl
 
     int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
 
-    if (reuse_port) {
-        std::cout << "Setting reuse port" << std::endl;
+    std::cout << "Setting reuse port" << std::endl;
 
-        int reuse_port_optval = 1;
+    int reuse_port_optval = 1;
 
-        auto set_reuse_port_res = setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse_port_optval, sizeof(reuse_port_optval));
+    auto set_reuse_port_res = setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse_port_optval, sizeof(reuse_port_optval));
 
-        if (set_reuse_port_res != 0) {
-            std::cout << "Cannot enable reuse port mode"<< std::endl;
-            return;
-        }
+    if (set_reuse_port_res != 0) {
+        std::cout << "Cannot enable reuse port mode"<< std::endl;
+        return;
+    }
 
-        // We may have custom reuse port load balancing algorithm 
-        if (true) {
-            std::cout << "Loading BPF to implement random UDP traffic distribution over available threads" << std::endl;
+    // We may have custom reuse port load balancing algorithm 
+    if (true) {
+        std::cout << "Loading BPF to implement random UDP traffic distribution over available threads" << std::endl;
 
-            struct sock_filter bpf_random_load_distribution[3] = {
-                /* A = (uint32_t)skb[0] */
-                { BPF_LD  | BPF_W | BPF_ABS, 0, 0, 0 },
-                /* A = A % mod */
-                { BPF_ALU | BPF_MOD, 0, 0, netflow_threads_per_port },
-                /* return A */
-                { BPF_RET | BPF_A, 0, 0, 0 },
-            };
+        struct sock_filter bpf_random_load_distribution[3] = {
+            /* A = (uint32_t)skb[0] */
+            { BPF_LD  | BPF_W | BPF_ABS, 0, 0, 0 },
+            /* A = A % mod */
+            { BPF_ALU | BPF_MOD, 0, 0, netflow_threads_per_port },
+            /* return A */
+            { BPF_RET | BPF_A, 0, 0, 0 },
+        };
 
-            struct sock_fprog bpf_programm;
+        struct sock_fprog bpf_programm;
 
-            bpf_programm.len    = 3;
-            bpf_programm.filter = bpf_random_load_distribution;
+        bpf_programm.len    = 3;
+        bpf_programm.filter = bpf_random_load_distribution;
 
-            // UDP support for this feature is available since Linux 4.5
-            int attach_filter_result = setsockopt(sockfd, SOL_SOCKET, SO_ATTACH_REUSEPORT_CBPF, &bpf_programm, sizeof(bpf_programm));
+        // UDP support for this feature is available since Linux 4.5
+        int attach_filter_result = setsockopt(sockfd, SOL_SOCKET, SO_ATTACH_REUSEPORT_CBPF, &bpf_programm, sizeof(bpf_programm));
 
-            if (attach_filter_result != 0) {
-                std::cout << "Can't attach reuse port BPF filter for port " << port_as_string
-                       << " errno: " << errno << " error: " << strerror(errno) << std::endl;
-                // It's not critical issue. We will use default distribution logic
-            } else { 
-                std::cout << "Successfully loaded reuse port BPF"<< std::endl;
-            }
+        if (attach_filter_result != 0) {
+            std::cout << "Can't attach reuse port BPF filter for port " << port_as_string
+                   << " errno: " << errno << " error: " << strerror(errno) << std::endl;
+            // It's not critical issue. We will use default distribution logic
+        } else { 
+            std::cout << "Successfully loaded reuse port BPF"<< std::endl;
         }
     }
 
@@ -98,11 +96,15 @@ void start_netflow_collector(const std::string& netflow_host, unsigned int netfl
 }
 
 int main() {
-    std::cout << "lol" << std::endl;
+    std::string host =  "0.0.0.0";
+    uint32_t port = 2056;
 
-    std::thread my_thread_1(start_netflow_collector, "0.0.0.0", 2056, true,  2);
+    uint32_t number_of_threads = 2;
 
-    std::thread my_thread_2(start_netflow_collector, "0.0.0.0", 2056, true,  2);
+    // Launch two thread to distribute load
+    std::thread my_thread_1(start_netflow_collector, host, port, number_of_threads);
+
+    std::thread my_thread_2(start_netflow_collector, host, port, number_of_threads);
     
     my_thread_1.join();
     my_thread_2.join();
